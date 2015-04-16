3d97d64224fe439cf6f337ef63feaec9865b04c3
cooler09 <zacharyjordan82@gmail.com>
2015-04-16 10:09:51 -0400

    faction and view model populated

---
+++ LoLApiChallenge/Controllers/BaseController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoLApiChallenge.Models;
+using Newtonsoft.Json;
+
+namespace LoLApiChallenge.Controllers
+{
+    public class BaseController : Controller
+    {
+        public int[] Demacia = {114,};
+        public int[] Noxus { get; set; }
+        public int[] Ionia { get; set; }
+        public int[] Piltover { get; set; }
+        public int[] BandleCity { get; set; }
+        public int[] Freljord { get; set; }
+        public int[] TheVoid { get; set; }
+        public int[] Shurima { get; set; }
+        public int[] MtTargon { get; set; }
+        public int[] ShadowIsles { get; set; }
+        public int[] Bilgewater { get; set; }
+        public int[] Independant { get; set; }
+        private static RootObject _data;
+        public RootObject Data {
+            get
+            {
+                if (_data != null) return _data;
+                using (StreamReader r =new StreamReader(AppDomain.CurrentDomain.BaseDirectory + @"\Content\snapshot.json"))
+                {
+                    string json = r.ReadToEnd();
+                    _data = JsonConvert.DeserializeObject<RootObject>(json);
+                }
+                return _data;
+            }
+        }
+    }
+}
