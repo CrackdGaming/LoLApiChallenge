0e6948b1b669c34790994a0e7f98ad4dc6f03c37
cooler09 <zacharyjordan82@gmail.com>
2015-04-16 12:12:32 -0400

    Faction data displaying on page along with images

--- LoLApiChallenge/Controllers/BaseController.cs
+++ LoLApiChallenge/Controllers/BaseController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -11,18 +12,20 @@ namespace LoLApiChallenge.Controllers
 {
     public class BaseController : Controller
     {
-        public int[] Demacia = {114,};
-        public int[] Noxus { get; set; }
-        public int[] Ionia { get; set; }
-        public int[] Piltover { get; set; }
-        public int[] BandleCity { get; set; }
-        public int[] Freljord { get; set; }
-        public int[] TheVoid { get; set; }
-        public int[] Shurima { get; set; }
-        public int[] MtTargon { get; set; }
-        public int[] ShadowIsles { get; set; }
-        public int[] Bilgewater { get; set; }
-        public int[] Independant { get; set; }
+        public int[] Bilgewater = { 105, 41, 104, 21, 111, 4 };
+        public int[] BandleCity = { 32, 42, 74, 85, 117, 78, 68, 17, 18, 45, 115};
+        public int[] BlackRose = { 7, 50,60, 8};
+        public int[] Demacia = { 114, 3, 86, 59, 10, 99, 78, 133, 102, 37, 44, 67, 5, 236 };
+        public int[] Freljord = { 34, 22, 79, 127, 20, 2, 113, 48, 23, 77, 106};
+        public int[] Ionia = { 103, 84, 39, 24, 43, 85, 64, 11, 98, 37, 16, 134, 77, 110, 62, 5, 238};
+        public int[] Independant = { 266, 12, 1, 63, 9, 40, 54, 57, 267, 76, 56, 107,13,35, 72, 143};
+        public int[] MtTargon = { 131, 89, 80};
+        public int[] Noxus = { 69, 122, 119, 55, 7, 25, 92, 14, 50, 91, 6, 8};
+        public int[] Piltover = { 51, 42, 81, 74, 126, 61, 254, 26};
+        public int[] Shurima = { 69, 75, 33, 58, 15, 101};
+        public int[] ShadowIsles = { 60, 28, 120, 30, 82, 412, 83};
+        public int[] TheVoid = { 31, 38, 121, 96, 90};
+        public int[] Zaun = { 53, 36, 27, 29, 6, 112, 19, 154};
         private static RootObject _data;
         public RootObject Data {
             get
--- LoLApiChallenge/Controllers/HomeController.cs
+++ LoLApiChallenge/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,7 +13,189 @@ namespace LoLApiChallenge.Controllers
         public ActionResult Index()
         {
             var model = new UrfDataModel{FullData = Data};
+            var facBilgewater = new FactionData
+            {
+                Name = "Bilgewater"
+            };
+            var facBandleCity = new FactionData
+            {
+                Name = "Bandle City"
+            };
+            var facBlackRose = new FactionData
+            {
+                Name = "Black Rose"
+            };
+            var facDemacia = new FactionData
+            {
+                Name = "Demacia"
+            };
+            var facFreljord = new FactionData
+            {
+                Name = "Freljord"
+            };
+            var facIonia = new FactionData
+            {
+                Name = "Ionia"
+            };
+            var facIndependant = new FactionData
+            {
+                Name = "Independant"
+            };
+            var facMtTargon = new FactionData
+            {
+                Name = "Mount Targon"
+            };
+            var facNoxus = new FactionData
+            {
+                Name = "Noxus"
+            };
+            var facPiltover = new FactionData
+            {
+                Name = "Piltover"
+            };
+            var facShurima = new FactionData
+            {
+                Name = "Shurimas Desert"
+            };
+            var facShadowIsles = new FactionData
+            {
+                Name = "Shadow Isles"
+            };
+            var facTheVoid = new FactionData
+            {
+                Name = "The Void"
+            };
+            var facZaun = new FactionData
+            {
+                Name = "Zaun"
+            };
+            foreach (var champ in Data.champions)
+            {
+                if (Bilgewater.Contains(champ.championId) )
+                {
+                    facBilgewater = AddTotals(champ.data.timesPicked, champ.data.stat.kills.totalKills,
+                        champ.data.stat.kills.totalAssists, champ.data.stat.kills.totalDeaths, facBilgewater);
+                }
+                if (BandleCity.Contains(champ.championId))
+                {
+                    facBandleCity = AddTotals(champ.data.timesPicked, champ.data.stat.kills.totalKills,
+                        champ.data.stat.kills.totalAssists, champ.data.stat.kills.totalDeaths, facBandleCity);
+                } 
+                if (BlackRose.Contains(champ.championId))
+                {
+                    facBlackRose = AddTotals(champ.data.timesPicked, champ.data.stat.kills.totalKills,
+                        champ.data.stat.kills.totalAssists, champ.data.stat.kills.totalDeaths, facBlackRose);
+                }
+                if (Demacia.Contains(champ.championId))
+                {
+                    facDemacia = AddTotals(champ.data.timesPicked, champ.data.stat.kills.totalKills,
+                        champ.data.stat.kills.totalAssists, champ.data.stat.kills.totalDeaths, facDemacia);
+                }
+                if (Freljord.Contains(champ.championId))
+                {
+                    facFreljord = AddTotals(champ.data.timesPicked, champ.data.stat.kills.totalKills,
+                        champ.data.stat.kills.totalAssists, champ.data.stat.kills.totalDeaths, facFreljord);
+                }
+                if (Ionia.Contains(champ.championId))
+                {
+                    facIonia = AddTotals(champ.data.timesPicked, champ.data.stat.kills.totalKills,
+                        champ.data.stat.kills.totalAssists, champ.data.stat.kills.totalDeaths, facIonia);
+                }
+                if (Independant.Contains(champ.championId))
+                {
+                    facIndependant = AddTotals(champ.data.timesPicked, champ.data.stat.kills.totalKills,
+                        champ.data.stat.kills.totalAssists, champ.data.stat.kills.totalDeaths, facIndependant);
+                }
+                if (MtTargon.Contains(champ.championId))
+                {
+                    facMtTargon = AddTotals(champ.data.timesPicked, champ.data.stat.kills.totalKills,
+                        champ.data.stat.kills.totalAssists, champ.data.stat.kills.totalDeaths, facMtTargon);
+                }
+                if (Noxus.Contains(champ.championId))
+                {
+                    facNoxus = AddTotals(champ.data.timesPicked, champ.data.stat.kills.totalKills,
+                        champ.data.stat.kills.totalAssists, champ.data.stat.kills.totalDeaths, facNoxus);
+                }
+                if (Piltover.Contains(champ.championId))
+                {
+                    facPiltover = AddTotals(champ.data.timesPicked, champ.data.stat.kills.totalKills,
+                        champ.data.stat.kills.totalAssists, champ.data.stat.kills.totalDeaths, facPiltover);
+                }
+                if (Shurima.Contains(champ.championId))
+                {
+                    facShurima = AddTotals(champ.data.timesPicked, champ.data.stat.kills.totalKills,
+                        champ.data.stat.kills.totalAssists, champ.data.stat.kills.totalDeaths, facShurima);
+                }
+                if (ShadowIsles.Contains(champ.championId))
+                {
+                    facShadowIsles = AddTotals(champ.data.timesPicked, champ.data.stat.kills.totalKills,
+                        champ.data.stat.kills.totalAssists, champ.data.stat.kills.totalDeaths, facShadowIsles);
+                }
+                if (TheVoid.Contains(champ.championId))
+                {
+                    facTheVoid = AddTotals(champ.data.timesPicked, champ.data.stat.kills.totalKills,
+                        champ.data.stat.kills.totalAssists, champ.data.stat.kills.totalDeaths, facTheVoid);
+                }
+                if (Zaun.Contains(champ.championId))
+                {
+                    facZaun = AddTotals(champ.data.timesPicked, champ.data.stat.kills.totalKills,
+                        champ.data.stat.kills.totalAssists, champ.data.stat.kills.totalDeaths, facZaun);
+                }
+            }
+
+            facBilgewater = AverageData(facBilgewater);
+            facBandleCity = AverageData(facBandleCity);
+            facBlackRose = AverageData(facBlackRose);
+            facDemacia = AverageData(facDemacia);
+            facFreljord = AverageData(facFreljord);
+            facIonia = AverageData(facIonia);
+            facIndependant = AverageData(facIndependant);
+            facMtTargon = AverageData(facMtTargon);
+            facNoxus = AverageData(facNoxus);
+            facPiltover = AverageData(facPiltover);
+            facShurima = AverageData(facShurima);
+            facShadowIsles = AverageData(facShadowIsles);
+            facTheVoid = AverageData(facTheVoid);
+            facZaun = AverageData(facZaun);
+
+            model.Factions.Add(facZaun);
+            model.Factions.Add(facBilgewater);
+            model.Factions.Add(facBandleCity);
+            model.Factions.Add(facBlackRose);
+            model.Factions.Add(facBandleCity);
+            model.Factions.Add(facDemacia);
+            model.Factions.Add(facFreljord);
+            model.Factions.Add(facIndependant);
+            model.Factions.Add(facIonia);
+            model.Factions.Add(facMtTargon);
+            model.Factions.Add(facNoxus);
+            model.Factions.Add(facPiltover);
+            model.Factions.Add(facShadowIsles);
+            model.Factions.Add(facShurima);
+            model.Factions.Add(facTheVoid);
+            model.Factions = model.Factions.OrderByDescending(f => f.Kills).ToList();
             return View(model);
+        }
+        public ActionResult GetImage(string name)
+        {
+            return File(AppDomain.CurrentDomain.BaseDirectory + @"\Content\Images\Factions\"+name + ".jpg", "image/jpeg");
+        }
+
+        public FactionData AddTotals(int totalGames, int totalKills, int totalAssists, int totalDeaths, FactionData data)
+        {
+            data.TotalGames += totalGames;
+            data.TotalKills += totalKills;
+            data.TotalAssists += totalAssists;
+            data.TotalDeaths += totalDeaths;
+            return data;
+        }
+
+        public FactionData AverageData(FactionData faction)
+        {
+            faction.Kills = faction.TotalKills / faction.TotalGames;
+            faction.Deaths = faction.TotalDeaths / faction.TotalGames;
+            faction.Assists = faction.TotalAssists / faction.TotalGames;
+            return faction;
         }
 
         public ActionResult Map()
