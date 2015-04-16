b0bd758c3f66ac68bad6f06dde632e0f857e4252
Trent Ahrens <tahrens@bhivelab.com>
2015-04-15 20:42:57 -0400

    header. footer, map

--- LoLApiChallenge/Controllers/HomeController.cs
+++ LoLApiChallenge/Controllers/HomeController.cs
@@ -10,21 +10,11 @@ namespace LoLApiChallenge.Controllers
     {
         public ActionResult Index()
         {
-
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Map()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
