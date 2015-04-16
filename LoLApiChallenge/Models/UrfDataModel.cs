6864df426c43720a719a31c4ba2994851a2c0625
cooler09 <zacharyjordan82@gmail.com>
2015-04-16 09:37:26 -0400

    Root Object and URF data models

---
+++ LoLApiChallenge/Models/UrfDataModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoLApiChallenge.Models
+{
+    public class UrfDataModel
+    {
+        public List<FactionData> Factions { get; set; }
+        public RootObject FullData { get; set; }
+        public List<ChampionData> Champions { get; set; }
+    }
+
+    public class FactionData
+    {
+        public byte[] Image { get; set; }
+        public string Name { get; set; }
+        public int Kills { get; set; }
+        public int Deaths { get; set; }
+        public int Assists { get; set; }
+    }
+
+    public class ChampionData
+    {
+        public byte[] Image { get; set; }
+        public string Name { get; set; }
+        public int Kills { get; set; }
+        public int Deaths { get; set; }
+        public int Assists { get; set; }
+        public int TotalWins{ get; set; }
+        public int TimesPicked { get; set; }
+        public int MostDeaths { get; set; }
+        
+    }
+}
